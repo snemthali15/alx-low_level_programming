9d08b873a3733dbc4925d1e61f6fc5c9737b4520
Snenhlanhla <sne.mtshali15@gmail.com>
2023-07-21 17:46:13 -0700

    Nested Loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - function to print alphabets in lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract thr last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int)
+{
+	int a;
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+}
