156fd1344bc4581e3b8bfa05bebab5a6816e316d
Snenhlanhla <sne.mtshali15@gmail.com>
2023-07-29 15:11:37 -0700

    0x05-pointers, arrays and strings

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,3 +1,4 @@
+int r;
 #include "main.h"
 
 /**
@@ -7,14 +8,12 @@
  */
 int main(void)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+       	r =  _islower('H');
+        _putchar(r + '0');
+        r =  _islower('o');
+        _putchar(r + '0');
+        r = _islower(108);
+        _putchar(r + '0');
+        _putchar('\n');
+        return (0);
 }
