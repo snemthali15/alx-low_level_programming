ccc1d8631a74655ef19e0f2ac017fb24938aba3e
Snenhlanhla <sne.mtshali15@gmail.com>
2023-07-29 17:52:49 -0700

    task 4,

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
 	int longi = 0;
 	int o;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
@@ -17,8 +17,8 @@ void print_rev(char *s)
 	s--;
 	for (o = longi; o > 0; o--)
 	{
-            	_putchar(*s);
-	   	s--;
+		_putchar(*s);
+		s--;
 	}
 	_putchar('\n');
 }
